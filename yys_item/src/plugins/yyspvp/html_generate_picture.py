d549d6a94be18090ec10de8d9d7da1f7f546ae2e
yyscbg <yyscbg@gmail.com>
2022-12-05 17:47:44 +0800

    update path

--- yys_item/src/plugins/yyspvp/html_generate_picture.py
+++ yys_item/src/plugins/yyspvp/html_generate_picture.py
@@ -92,7 +92,7 @@ def run_image(datas, title, _type=None):
             table_content += __content
 
     image_path = os.path.join(base_path, "image")
-    image_file = "temp" + str(time.time() * 1000)[:13] + "." + ".png"
+    image_file = "temp" + str(time.time() * 1000)[:13] + ".png"
     save_image_file = os.path.join(image_path, "temp_cut_" + str(time.time() * 1000)[:13] + ".png")
 
     try:
--- yys_item/src/plugins/yyspvp/temp.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-"""
-@Time: 2022/12/5下午5:09
-@Author: kasper
-@Email: yyscbg@gmail.com
-@FileName: temp.py
-@Detail: 
-"""
